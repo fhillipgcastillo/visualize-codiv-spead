26ce638ec5ddc7b7d344edacf678814788e88d5c
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-05-24 19:35:41 -0400

    add axious and moment, get and display covid from DR graphs

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend  } from "recharts";
+import axios from 'axios';
 
 export default function Main(props) {
+  const [spread, setSpread] = React.useState([]);
+  const apiUrl = "https://api.covid19api.com/country/dominican-republic";
+
+  const handleGetData = () => axios.get(apiUrl, {contentType: "json"}).then(res=> {
+    console.log('res api', res)
+    setSpread(res.data);
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {spread.length > 0 ? (
+        <LineChart width={1200} height={800}  data={spread}>
+          <Line type="monotone" dataKey="Confirmed" stroke="#8884d8"/>
+          <Line type="monotone" dataKey="Active" />
+          <Line type="monotone" dataKey="Recovered" />
+          <Line type="monotone" dataKey="Deaths" />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis />
+          <YAxis dataKey="Confirmed" />
+        </LineChart>
+      ) : (
+        <button onClick={handleGetData}>Load Charts</button>
+      )}
     </div>
   );
 }
